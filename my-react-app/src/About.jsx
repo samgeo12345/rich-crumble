ddc406dc77e997b748eaf7fa106ffb8e41f2cbd1
samgeo12345 <samgeoj12d@gmail.com>
2025-03-30 00:14:23 +0530

    updated

--- my-react-app/src/About.jsx
+++ my-react-app/src/About.jsx
@@ -8,8 +8,6 @@ function About(){
             <br /><br />
             At Rich Crumble, we take pride in crafting exquisite cakes using only the finest ingredients, ensuring every creation is soft, moist, and bursting with flavor. Whether you crave the rich indulgence of chocolate cake, the refreshing zest of lemon vanilla, the floral elegance of rose vanilla, or the fruity delight of black currant, we have something special for every occasion.
             <br /><br />
-            From classic favorites to unique signature cakes, our handcrafted delights are perfect for birthdays, weddings, anniversaries, and all of life’s special moments.
-            <br /><br />
             Indulge in the magic of Rich Crumble, where every cake is a masterpiece. 
             </p>
             </div>
